5d19ea3d3be09ffa7e3ca9c5b548140562c9647a
PavilionDV7 <okihurisennsyu@gmail.com>
2014-02-11 12:45:44 +0900

    LeapMotionによるカメラの操作と握るモーションによる球体生成を追加

---
+++ Assets/MyAssets/Scripts/GrabMotion.cs
@@ -0,0 +1,150 @@
+﻿using UnityEngine;
+using System.Collections;
+using Leap;
+
+public class GrabMotion : MonoBehaviour
+{
+
+    public GameObject OriginalSphere;
+    GameObject AddForceObject;
+    Controller m_leapController;
+    float m_lastBlastTime = 0.0f;
+
+    GameObject m_carriedObject;
+    bool m_handOpenThisFrame = false;
+    bool m_handOpenLastFrame = false;
+
+    // Use this for initialization
+    void Start()
+    {
+        m_leapController = new Controller();
+    }
+
+    // gets the hand furthest away from the user (closest to the screen).
+    Hand GetForeMostHand()
+    {
+        Frame f = m_leapController.Frame();
+        Hand foremostHand = null;
+        float zMax = -float.MaxValue;
+        for (int i = 0; i < f.Hands.Count; ++i)
+        {
+            float palmZ = f.Hands[i].PalmPosition.ToUnityScaled().z;
+            if (palmZ > zMax)
+            {
+                zMax = palmZ;
+                foremostHand = f.Hands[i];
+            }
+        }
+
+        return foremostHand;
+    }
+
+    // 手が開いている場合の処理.
+    void OnHandOpen(Hand h)
+    {
+        if (AddForceObject == null)
+            return;
+        AddForceObject.rigidbody.AddForce(transform.forward * 1000.0f);
+        m_carriedObject = null;
+    }
+
+    // 手が閉じている場合の処理.
+    void OnHandClose(Hand h)
+    {
+        AddForceObject = Instantiate(OriginalSphere) as GameObject;
+        AddForceObject.transform.position = transform.position + transform.forward;
+        
+        // look for an object to pick up.
+        // Rayを飛ばしHitしたオブジェクトを「移動させるオブジェクト」として記録する
+        //RaycastHit hit;
+        //if (Physics.SphereCast(new Ray(transform.position + transform.forward * 2.0f, transform.forward), 2.0f, out hit))
+        //{
+        //    m_carriedObject = hit.collider.gameObject;
+        //}
+    }
+
+    bool IsHandOpen(Hand h)
+    {
+        return h.Fingers.Count > 1;
+    }
+
+    // processes character camera look based on hand position.
+    // 手の左右方向に合わせてカメラをY軸回転させる.
+    void ProcessLook(Hand hand)
+    {
+        float rotThresholdX = 1.0f;
+        float rotThresholdY = 3.0f;
+        float handX = hand.PalmPosition.ToUnityScaled().x;
+        float handY = hand.PalmPosition.ToUnityScaled().y;
+        //Debug.Log(Mathf.Abs(handY));
+        //Debug.Log(handY);
+        if (Mathf.Abs(handX) > rotThresholdX)
+        {
+            Camera.main.transform.Rotate(Vector3.up, handX * 2.0f);
+        }
+    }
+
+    void MoveCharacter(Hand hand)
+    {
+        if (hand.PalmPosition.ToUnityScaled().z > 0)
+        {
+            Camera.main.transform.position += Camera.main.transform.forward * 0.1f;
+        }
+
+        if (hand.PalmPosition.ToUnityScaled().z < -1.0f)
+        {
+            Camera.main.transform.position -= Camera.main.transform.forward * 0.04f;
+        }
+    }
+
+    // Determines if any of the hand open/close functions should be called.
+    // 手が開いているか閉じているかに応じてのコールバック（反応）を定義.
+    void HandCallbacks(Hand h)
+    {
+        if (m_handOpenThisFrame && m_handOpenLastFrame == false)
+        {
+            Debug.Log("Release Grab");
+            OnHandOpen(h);
+        }
+
+        if (m_handOpenThisFrame == false && m_handOpenLastFrame == true)
+        {
+            Debug.Log("Is Grab");
+            OnHandClose(h);
+        }
+    }
+
+    // if we're carrying an object, perform the logic needed to move the object
+    // with us as we walk (or pull it toward us if it's far away).
+    // 掴んだオブジェクトを移動（運ぶ）させる.
+    void MoveCarriedObject()
+    {
+        if (m_carriedObject != null)
+        {
+            Vector3 targetPos = transform.position + new Vector3(transform.forward.x, 0, transform.forward.z) * 5.0f;
+            Vector3 deltaVec = targetPos - m_carriedObject.transform.position;
+            if (deltaVec.magnitude > 0.1f)
+            {
+                m_carriedObject.rigidbody.velocity = (deltaVec) * 10.0f;
+            }
+            else
+            {
+                m_carriedObject.rigidbody.velocity = Vector3.zero;
+            }
+        }
+    }
+
+    void FixedUpdate()
+    {
+        Hand foremostHand = GetForeMostHand();
+        if (foremostHand != null)
+        {
+            m_handOpenThisFrame = IsHandOpen(foremostHand);
+            ProcessLook(foremostHand);
+            //MoveCharacter(foremostHand);
+            HandCallbacks(foremostHand);
+            MoveCarriedObject();
+        }
+        m_handOpenLastFrame = m_handOpenThisFrame;
+    }
+}
---
+++ Assets/MyAssets/Scripts/MouseLook.cs
@@ -0,0 +1,105 @@
+﻿using UnityEngine;
+using System.Collections;
+using Leap;
+
+public class MouseLook : MonoBehaviour
+{
+    public enum RotationAxes { MouseXAndY = 0, MouseX = 1, MouseY = 2 }
+    public RotationAxes axes = RotationAxes.MouseXAndY;
+    public float sensitivityX = 15F;
+    public float sensitivityY = 15F;
+
+    public float minimumX = -360F;
+    public float maximumX = 360F;
+
+    public float minimumY = -60F;
+    public float maximumY = 60F;
+
+    Controller controller; // leap Controller
+
+    float rotationY = 0F;
+
+    int lastFrameFingerCount = 0;
+
+    void Update()
+    {
+        Frame frame = controller.Frame();
+
+        float xInput = Input.GetAxis("Mouse X");
+        float yInput = Input.GetAxis("Mouse Y");
+
+        Vector3 averageFingerVelocity = Vector3.zero;
+
+        for (int i = 0; i < frame.Fingers.Count; ++i)
+        {
+            averageFingerVelocity += frame.Fingers[i].TipVelocity.ToUnityScaled();
+        }
+
+        averageFingerVelocity /= frame.Fingers.Count;
+
+        float zFactor = Mathf.Abs(averageFingerVelocity.z);
+        float xFactor = averageFingerVelocity.x * averageFingerVelocity.x + 1.0f;
+        float yFactor = averageFingerVelocity.y * averageFingerVelocity.y + 1.0f;
+
+        bool notPullingOrPushing = zFactor < xFactor && zFactor < yFactor;
+        if (frame.Hands.Count > 0)
+        {
+            Vector3 palmPosition = frame.Hands[0].PalmPosition.ToUnityScaled();
+            Vector3 palmVelocity = frame.Hands[0].PalmVelocity.ToUnityScaled();
+            if (palmPosition.z > 0.0 && palmVelocity.magnitude < 8.0f && frame.Fingers.Count > 2)
+            {
+                xInput = averageFingerVelocity.x * Mathf.Abs(averageFingerVelocity.x);
+                xInput *= 0.1f;
+                yInput = averageFingerVelocity.y * Mathf.Abs(averageFingerVelocity.y);
+                yInput *= 0.1f;
+            }
+            // perform a pull.
+            if (frame.Fingers.Count < 1)
+            {
+                GameObject[] pullboxes = GameObject.FindGameObjectsWithTag("Pullable");
+                for (int i = 0; i < pullboxes.Length; ++i)
+                {
+                    if (pullboxes[i].renderer.isVisible)
+                    {
+                        Vector3 pullObjectToMe = transform.position - pullboxes[i].transform.position;
+                        pullboxes[i].rigidbody.velocity += pullObjectToMe.normalized * (3.0f / Mathf.Max(1.0f, pullObjectToMe.magnitude));
+                    }
+                }
+            }
+        }
+
+        if (axes == RotationAxes.MouseXAndY)
+        {
+
+            float rotationX = transform.localEulerAngles.y + xInput * sensitivityX;
+
+            rotationY += yInput * sensitivityY;
+            rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
+
+            transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
+        }
+        else if (axes == RotationAxes.MouseX)
+        {
+            transform.Rotate(0, xInput * sensitivityX, 0);
+        }
+        else
+        {
+            rotationY += yInput * sensitivityY;
+            rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
+
+            transform.localEulerAngles = new Vector3(-rotationY, transform.localEulerAngles.y, 0);
+        }
+        lastFrameFingerCount = frame.Fingers.Count;
+    }
+
+    void Start()
+    {
+        // Make the rigid body not change rotation
+        if (rigidbody)
+            rigidbody.freezeRotation = true;
+
+        controller = new Controller(); // init leap controller
+        UnityEngine.Screen.showCursor = false;
+        UnityEngine.Screen.lockCursor = true;
+    }
+}
